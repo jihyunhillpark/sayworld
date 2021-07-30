9c436d2a0e661d1bb68694b62f6d5dfa876b87f1
subin <ansubin2004@gmail.com>
2021-07-30 11:45:43 +0900

    style: 파일 이름 수정
    vue 파일 컨벤션 Camel Case로 통일시키기 위해서 수정

---
+++ frontend/src/views/main/Main.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-container class="main-wrapper">
+    <main-header :height="`70px`" @openLoginDialog="onOpenLoginDialog" />
+    <el-container class="main-container">
+      <el-aside class="hide-on-small" width="240px">
+        <main-sidebar :width="`240px`" />
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+    <main-footer :height="`110px`" />
+  </el-container>
+  <login-dialog
+    :open="loginDialogOpen"
+    @closeLoginDialog="onCloseLoginDialog"
+  />
+</template>
+<style>
+@import "https://unpkg.com/element-plus/lib/theme-chalk/index.css";
+@import "./main.css";
+@import "../../common/css/common.css";
+@import "../../common/css/element-plus.css";
+</style>
+<script>
+import LoginDialog from "../../components/main/login-dialog";
+import MainHeader from "../../components/main/main-header";
+import MainSidebar from "../../components/main/main-sidebar";
+import MainFooter from "../../components/main/main-footer";
+
+export default {
+  name: "Main",
+  components: {
+    MainHeader,
+    MainSidebar,
+    MainFooter,
+    LoginDialog
+  },
+  data() {
+    return {
+      loginDialogOpen: false
+    };
+  },
+  methods: {
+    onOpenLoginDialog() {
+      this.loginDialogOpen = true;
+    },
+    onCloseLoginDialog() {
+      this.loginDialogOpen = false;
+    }
+  }
+};
+</script>
