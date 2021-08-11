d19fecb4ec1acc87537ec768ee8d44b8a0c89ff2
jshagold <jshagold@gmail.com>
2021-08-11 17:24:37 +0900

    fix: SimpMessagingTemplate 오류 수정
    빈 어노테이션 관련 오류 수정

--- backend/src/main/java/com/ssafy/config/StompWebSocketConfig.java
+++ backend/src/main/java/com/ssafy/config/StompWebSocketConfig.java
@@ -1,9 +1,13 @@
 package com.ssafy.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@EnableWebSocketMessageBroker
+@Configuration
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
