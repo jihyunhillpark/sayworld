7fa2eb7ca2e8df5ed0e4bd17816cc44605d87bbc
jshagold <jshagold@gmail.com>
2021-08-13 14:40:37 +0900

    feat: 블로그 프론트페이지 개발중
    블로그 프론트 페이지 개발중

--- frontend/src/components/mypage/MyBlog.vue
+++ frontend/src/components/mypage/MyBlog.vue
@@ -1,11 +1,31 @@
 <template>
-<a> 내 블로그 페이지</a>
+  <div>
+    <a> 내 블로그 페이지</a>
+
+  </div>
+
 </template>
 
 <script>
-export default {
-  name: "MyBlog"
-}
+  import {onMounted, reactive} from 'vue'
+  import {useStore} from 'vuex'
+
+  export default {
+    name: "MyBlog",
+
+    setup(){
+      const store = useStore()
+      const blogList = reactive({
+        blogs: [],
+      })
+
+      onMounted(() => {
+        store.dispatch('root/blogList')
+          .then()
+      })
+    }
+
+  }
 </script>
 
 <style scoped>
